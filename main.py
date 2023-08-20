1dbe88edd0841ab6089d428404159219ce0ad6f3
gusanitor8 <gusanitor8@gmail.com>
2023-08-19 23:59:55 -0600

    menu

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from shuntingYard.syv2 import getPostfixRegex
 from lab4.AFNBuilder import AFNBuilder
+from lab4.AFNReader import AFNReader
 
-def main():
-    postfixArr = getPostfixRegex()
-    
+postfixArr = getPostfixRegex()
+
+def main():        
     for postfix in postfixArr:
         afn = AFNBuilder(postfix).build()
         afn.terminal.printAFN(postfix)
@@ -11,5 +12,28 @@ def main():
 def testing():
     afn = AFNBuilder('ab|?t.').build()
     afn.terminal.printAFN('test')
+
+def menu():
+    menu = True
+
+    while menu:
+        print("Eliga su expresion regular o ingrese 'q' para salir: ")
+        for posfixRegex in range(len(postfixArr)):
+            print(str(posfixRegex) + ". " + postfixArr[posfixRegex])            
+
+            try:
+                option = int(input("Opcion: "))
+                if option == 'q':
+                    menu = False
+                    break
+                            
+                afn = AFNBuilder(postfixArr[option]).build()
+                string = input("Ingrese la cadena a evaluar: ")
+                reader = AFNReader(afn, string)
+                print(reader.read())
+
+            except ValueError:
+                pass
+
 
 main()
